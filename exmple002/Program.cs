f4b51fc6accf82ee786909e5f34c96e3d1194c4f
Пётр <dimok392@mail.ru>
2022-04-17 05:25:44 +0300

    exmple002

---
+++ exmple002/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = -9;
+int b = -3;
+if (a>b)
+    {
+        Console.WriteLine($"Значение {a} больше чем {b} ");
+    }
+else
+    {
+     Console.WriteLine($"Значение {b} больше чем {a} ");
+    }
+
